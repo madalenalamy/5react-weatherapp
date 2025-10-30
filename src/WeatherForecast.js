6a74fe6624529199da5682b156966d036d9bd8f4
Madalena Lamy <lamymadalena@gmail.com>
2025-10-30 14:20:20 +0000

    forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,57 @@
+
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+  function load() {
+    const apiKey = "79e2b848de54da3deo0aafeff8t7fa08";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        {" "}
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  {" "}
+                  <WeatherForecastDay
+                    key={index}
+                    data={dailyForecast}
+                    unit={props.unit}
+                  />{" "}
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+    return null;
+  }
+}
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,44 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = props.data.temperature.maximum;
+    if (props.unit === "fahrenheit") {
+      temperature = (temperature * 9) / 5 + 32;
+    }
+    return Math.round(temperature);
+  }
+
+  function minTemperature() {
+    let temperature = props.data.temperature.minimum;
+    if (props.unit === "fahrenheit") {
+      temperature = (temperature * 9) / 5 + 32;
+    }
+    return Math.round(temperature);
+  }
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+  return (
+    <div>
+      <div className="WeatherForecast-day">{day()}</div>{" "}
+      <WeatherIcon code={props.data.condition.icon} size={36} />
+      <div className="weatherForecast-temperatures">
+        {" "}
+        <span className="weatherForecast-temperature-max">
+          {" "}
+          {maxTemperature()}
+        </span>{" "}
+        |
+        <span className="weatherForecast-temperature-min">
+          {" "}
+          {minTemperature()}
+        </span>
+      </div>
+    </div>
+  );
+}
